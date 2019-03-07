90211ff9dda3732b722e12fe1b5ff6aea8985552
lore0223 <loremma23@gmail.com>
2019-03-07 10:18:34 -0600

    en este commit cambiamos las rutas de los html para la navegación de los html

--- src/javascripts/login.js
+++ src/javascripts/login.js
@@ -18,7 +18,7 @@ const uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: 'main.html',
+    signInSuccessUrl: '../html/user-information.html',
     signInOptions: [
        //Leave the lines as is for the providers you want to offer your users.
       //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
