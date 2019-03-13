9650cb7ef7f977d5c4fe05e8e267eee7a1263373
Pau-za <molo_pau@hotmail.com>
2019-03-12 18:51:20 -0600

    quita código comentado de form.js y main.js

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,31 +1,3 @@
-// let mainApp = {};
-// (() => {
-//     const firebase= appFireBase;
-//     let uid = null;
-//     firebase.auth().onAuthStateChanged(function (user) {
-//         if (user) {
-//             // Usuario está registrado
-//             uid = user.uid;
-//         } else {
-//             //redirecciona a la pagina para login
-//             uid = null;
-//             window.location.replace("../html/login.html")
-//         }
-//     });
-
-//     const logOut = () => {
-//         firebase.auth().signOut();
-//     }
-//     mainApp.logOut = logOut;
-// })();
-
-// const logOutButton = document.getElementById('log-out-button');
-// logOutButton.addEventListener('click', ()=>{
-//     mainApp.logOut();
-// })
-
-// //holi!
-
 let mainApp = {};
 let data = {};
 let uid = '';
